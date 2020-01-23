f05804627dfb8fed0773551d4514d0aa2cabb9bf
Ben Spark <bhrugen1990@gmail.com>
2020-01-22 19:30:32 -0600

    Upsert Post and Delete

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -40,6 +40,26 @@ namespace BookListMVC.Controllers
             return View(Book);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    //create
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
+        }
 
         #region API Calls
         [HttpGet]
